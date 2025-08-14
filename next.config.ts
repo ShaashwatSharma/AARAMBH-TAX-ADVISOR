5f4591b240e80903fd749be2890946a45d3980be
Shaashwat Sharma <shaashwatsharma12023@gmail.com>
2025-08-14 11:11:47 +0530

    init

--- next.config.ts
+++ next.config.ts
@@ -4,5 +4,9 @@ const nextConfig: NextConfig = {
   devIndicators: false,
   /* config options here */
 };
-
+module.exports = {
+  async redirects() {
+    return [{ source: '/', destination: '/home', permanent: false }];
+  },
+};
 export default nextConfig;
